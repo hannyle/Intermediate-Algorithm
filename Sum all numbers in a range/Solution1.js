77a609d961d172fc6e65935fc456bb7c9e5d6633
Hang Le <hannyle91@gmail.com>
2018-03-02 19:11:23 +0200

    Update Solution1.js

--- Sum all numbers in a range/Solution1.js
+++ Sum all numbers in a range/Solution1.js
@@ -1,11 +1,10 @@
 function sumAll(arr) {
-  var sum = arguments[0][0] + arguments[0][1];
-  
-  var maxNum = Math.max(arguments[0][0], arguments[0][1]);
-  
- var minNum = Math.min(arguments[0][0], arguments[0][1]);
+  var sum = arguments[0][0] + arguments[0][1];  
+  var maxNum = Math.max(arguments[0][0], arguments[0][1]);  
+  var minNum = Math.min(arguments[0][0], arguments[0][1]);
   var i;
   var newArr =[];
+  
   for(i = minNum+1; i<= maxNum; i++){
     if(i>minNum && i< maxNum) {
       newArr.push(i);
@@ -14,6 +13,5 @@ function sumAll(arr) {
   }
   return sum;
 }
-
 sumAll([4, 1]);
 //result 10
