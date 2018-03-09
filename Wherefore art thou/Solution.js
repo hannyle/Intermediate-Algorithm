daf2b38bec0755fe9cbfd6bfbb9279326f956cba
Hang Le <hannyle91@gmail.com>
2018-03-09 18:18:04 +0200

    Create Solution.js

---
+++ Wherefore art thou/Solution.js
@@ -0,0 +1,36 @@
+function whatIsInAName(collection, source) {
+  
+  var arr = [];
+  //function to get the array of Keys of an object
+  function keyArr (obj){
+    var objKeyArr = Object.keys(obj);
+    return objKeyArr;
+  }
+  //get the Key array of argument 'source'
+  var sourceKeyArr = keyArr(source);
+  
+  var collectKeyArr = [];
+  var collectValueArr = []; 
+  
+  var match;
+  
+  for(var i=0; i<collection.length; i++){
+    //get the Key array of argument 'collection'
+     collectKeyArr = keyArr(collection[i]);    
+      for(var j =0; j<sourceKeyArr.length; j++){
+        //check whether the 'source' Keys exist in 'collection' Keys, and whether the property values are matched with the same Keys, here is 'sourceKeyArr[j]'
+        //source[sourceKeyArr[j]]: get the property value of 'source' Keys[j]
+        //collection[i][sourceKeyArr[j]]: get the property value of 'collection' Keys[j]
+        if(collectKeyArr.indexOf(sourceKeyArr[j]) !== -1 && source[sourceKeyArr[j]] == collection[i][sourceKeyArr[j]]){        
+          match = true;
+        } 
+        else {match = false;}        
+  }    
+    if(match == true){
+      arr.push(collection[i]);
+    }
+ }   
+   return arr;  
+}
+
+whatIsInAName([{ "a": 2, "b": 2 }, { "a": 1 }, { "a": 1, "b": 2, "c": 2 }], { "a": 1, "b": 2 });
