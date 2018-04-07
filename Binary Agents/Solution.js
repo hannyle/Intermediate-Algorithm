2aac338f8399d9c5899cd4c69ba85f03e1bc5995
Hang Le <hannyle91@gmail.com>
2018-04-07 19:41:31 +0300

    Solution

---
+++ Binary Agents/Solution.js
@@ -0,0 +1,12 @@
+function binaryAgent(str) {
+  var arr = str.split(' ');
+  var newArr = [];
+  for(var i=0;i<arr.length; i++){
+    var a = parseInt(arr[i],2);
+    var newStr = String.fromCharCode(a);
+    newArr.push(newStr);
+  }
+  return newArr.join('');
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
